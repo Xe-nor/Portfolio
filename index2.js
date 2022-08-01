45747245cd04dfc3469e43fbc79438750c65d711
Xe-nor <amarendra.amar.dash@gmail.com>
2022-08-01 21:32:19 +0530

    changes to form

--- index2.js
+++ index2.js
@@ -62,3 +62,22 @@ function openmenu() {
 function closemenu() {
   sidemeu.style.right = "-200px";
 }
+
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzwK-kRsdCg21ndt7xIDaKY7DLcnw0OahGlkhxbfk2qtfPZY0hhwA3ScjaswAu_PnV2/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg=document.getElementById("msg")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully!"
+      setTimeout(function () {
+        msg.innerHTML=""
+      }, 5000)
+      form.reset()
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
