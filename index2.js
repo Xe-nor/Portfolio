80b4ad3cb693ebfa1862d85140dd184a6f51ab50
Xe-nor <amarendra.amar.dash@gmail.com>
2022-08-02 17:11:00 +0530

    form

--- index2.js
+++ index2.js
@@ -66,18 +66,55 @@ function closemenu() {
 
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbzwK-kRsdCg21ndt7xIDaKY7DLcnw0OahGlkhxbfk2qtfPZY0hhwA3ScjaswAu_PnV2/exec";
-const form = document.forms["submit-to-google-sheet"];
+const forrm = document.forms["submit-to-google-sheet"];
 const msg=document.getElementById("msg")
 
-form.addEventListener("submit", (e) => {
+forrm.addEventListener("submit", (e) => {
   e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+  fetch(scriptURL, { method: "POST", body: new FormData(forrm) })
     .then((response) => {
       msg.innerHTML = "Message sent successfully!"
       setTimeout(function () {
         msg.innerHTML=""
       }, 5000)
-      form.reset()
+      forrm.reset()
     })
     .catch((error) => console.error("Error!", error.message));
 });
+
+
+ var form = document.getElementById("my-form");
+
+ async function handleSubmit(event) {
+   event.preventDefault();
+   var status = document.getElementById("my-form-status");
+   var data = new FormData(event.target);
+   fetch(event.target.action, {
+     method: form.method,
+     body: data,
+     headers: {
+       Accept: "application/json",
+     },
+   })
+     .then((response) => {
+       if (response.ok) {
+         status.innerHTML = "";
+         form.reset();
+       } else {
+         response.json().then((data) => {
+           if (Object.hasOwn(data, "errors")) {
+             status.innerHTML = data["errors"]
+               .map((error) => error["message"])
+               .join(", ");
+           } else {
+             status.innerHTML =
+               "Oops! There was a problem submitting your form";
+           }
+         });
+       }
+     })
+     .catch((error) => {
+       status.innerHTML = "Oops! There was a problem submitting your form";
+     });
+ }
+ form.addEventListener("submit", handleSubmit);
